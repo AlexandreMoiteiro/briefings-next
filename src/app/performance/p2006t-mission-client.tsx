18f297697a89fe43170ebb1988664e51911c0a8e
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-07-20 22:58:17 +0100

    Use independently editable P2006T taxi time

--- src/app/performance/p2006t-mission-client.tsx
+++ src/app/performance/p2006t-mission-client.tsx
@@ -1,1 +1,1 @@
-export { P2006TMissionClientV7 as P2006TMissionClient } from "./p2006t-mission-client-v7";
+export { P2006TMissionClientV8 as P2006TMissionClient } from "./p2006t-mission-client-v8";
