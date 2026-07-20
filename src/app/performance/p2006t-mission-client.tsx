a04e074c519f948a156d13d54894f39ea3e66a46
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-07-20 21:40:16 +0100

    Use table-based P2006T fuel planning UI

--- src/app/performance/p2006t-mission-client.tsx
+++ src/app/performance/p2006t-mission-client.tsx
@@ -1,1 +1,1 @@
-export { P2006TMissionClientV4 as P2006TMissionClient } from "./p2006t-mission-client-v4";
+export { P2006TMissionClientV5 as P2006TMissionClient } from "./p2006t-mission-client-v5";
