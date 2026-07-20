01a8106f1bc092e183d3577d2ee22e2759dcccc9
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-07-20 21:43:28 +0100

    Use stabilized P2006T fuel planning table

--- src/app/performance/p2006t-mission-client.tsx
+++ src/app/performance/p2006t-mission-client.tsx
@@ -1,1 +1,1 @@
-export { P2006TMissionClientV5 as P2006TMissionClient } from "./p2006t-mission-client-v5";
+export { P2006TMissionClientV6 as P2006TMissionClient } from "./p2006t-mission-client-v6";
