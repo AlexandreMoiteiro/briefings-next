9359bee072a66291d4326caae946eed4068a0708
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-07-20 16:29:36 +0100

    Use hour and minute P2006T duration fields

--- src/app/performance/p2006t-mission-client.tsx
+++ src/app/performance/p2006t-mission-client.tsx
@@ -1,1 +1,1 @@
-export { P2006TMissionClientV3 as P2006TMissionClient } from "./p2006t-mission-client-v3";
+export { P2006TMissionClientV4 as P2006TMissionClient } from "./p2006t-mission-client-v4";
