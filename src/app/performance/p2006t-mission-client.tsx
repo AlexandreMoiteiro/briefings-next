d76f25777d374a99a68e7302b563d626588927ab
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-07-30 18:31:00 +0100

    Use tracked P2006T performance client

--- src/app/performance/p2006t-mission-client.tsx
+++ src/app/performance/p2006t-mission-client.tsx
@@ -1,1 +1,1 @@
-export { P2006TMissionClientV8 as P2006TMissionClient } from "./p2006t-mission-client-v8";
+export { P2006TMissionClientV9 as P2006TMissionClient } from "./p2006t-mission-client-v9";
