0c8a2b61d48d145c12a4ce9b8ecf19d9e136dbbf
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-08-03 19:19:43 +0100

    Use separate P2006T download controls

--- src/app/performance/p2006t-mission-client.tsx
+++ src/app/performance/p2006t-mission-client.tsx
@@ -1,1 +1,1 @@
-export { P2006TMissionClientV9 as P2006TMissionClient } from "./p2006t-mission-client-v9";
+export { P2006TMissionClientV10 as P2006TMissionClient } from "./p2006t-mission-client-v10";
