e17da7df62129e3e6cfe46e60f1b868db5c26625
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-09-06 13:31:33 +0100

    Use standalone C152 performance client

--- src/app/performance/performance-router-client.tsx
+++ src/app/performance/performance-router-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { C152ClientV2 } from "./c152-client-v2";
+import { C152ClientV3 } from "./c152-client-v3";
 import { P2006TMissionClient } from "./p2006t-mission-client";
 import { StandardAircraftClientV3 } from "./standard-aircraft-client-v3";
 
@@ -46,7 +46,7 @@ export function PerformanceRouterClient() {
       {mode === "PA28" ? (
         <StandardAircraftClientV3 aircraft="Piper PA-28" />
       ) : null}
-      {mode === "C152" ? <C152ClientV2 /> : null}
+      {mode === "C152" ? <C152ClientV3 /> : null}
     </div>
   );
 }
