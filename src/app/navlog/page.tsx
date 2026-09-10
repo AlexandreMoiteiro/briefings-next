1a76d7e9d229fefb6c0dfaa41e5b6a1069b7e0ef
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-09-10 04:53:42 +0100

    Use stable NavLog client directly

--- src/app/navlog/page.tsx
+++ src/app/navlog/page.tsx
@@ -1,5 +1,5 @@
-import { NavlogClientV5 } from "./navlog-client-v5";
+import { NavlogClient } from "./navlog-client";
 
 export default function NavlogPage() {
-  return <NavlogClientV5 />;
+  return <NavlogClient />;
 }
