fbf9c576907f6b72ca607be77de859a31541494e
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2026-09-10 06:20:22 +0100

    Show NavLog reference points by default

--- src/lib/navlog.ts
+++ src/lib/navlog.ts
@@ -190,7 +190,7 @@ export const navlogDefaultSetup: NavlogSetupForm = {
   rocFpm: defaultProfile.rocFpm,
   rodFpm: defaultProfile.rodFpm,
   defaultAltitude: defaultProfile.defaultAltitude,
-  showReferencePoints: false,
+  showReferencePoints: true,
   referenceLayers: navlogReferenceLayers,
   showAirways: true,
   showOpenAip: true,
